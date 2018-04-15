863054ee69e5435856e87fc7e1980e842264566c
Luiz Felipe Souza <luizsci42@gmail.com>
2018-04-15 19:56:45 -0300

    Notificação adicionada
    Signed-off-by: Luiz Felipe Souza <luizsci42@gmail.com>

--- app/src/main/java/nevicelabs/pibiti/MainActivity.java
+++ app/src/main/java/nevicelabs/pibiti/MainActivity.java
@@ -1,6 +1,7 @@
 package nevicelabs.pibiti;
 
 import android.Manifest;
+import android.app.Notification;
 import android.app.PendingIntent;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
@@ -9,6 +10,7 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -47,6 +49,7 @@ public class MainActivity extends AppCompatActivity implements
     private static final String CHAVE_INSCRITO = "inscrito";
     private static final int BLUETOOTH_ENABLE_REQUEST_ID = 1;
     private boolean inscrito = false;
+    private String mensagemRecebida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -149,7 +152,10 @@ public class MainActivity extends AppCompatActivity implements
                         if(status.isSuccess()) {
                             Log.i(TAG, "Inscrito com sucesso");
                             startService(getBackgroundSubscribeServiceIntent());
+                            MensagensService service = new MensagensService();
                             inscrito = true;
+                            mensagemRecebida = service.getMensagem();
+                            mensagem.setText(mensagemRecebida);
                         } else {
                             Log.e(TAG, "Falha na operação. Erro: " +
                                     NearbyMessagesStatusCodes
